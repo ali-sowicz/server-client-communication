091dafa86e1dac83a4f084f7d8b6819a3e7efba2
ali-sowicz <94705023+ali-sowicz@users.noreply.github.com>
2022-04-27 15:08:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+            int port = 80;
+            byte[] bufor;
+            try
+            {
+                string text = System.IO.File.ReadAllText(@"C:\Users\alicj\source\repos\Client\client.txt");
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(text);
+
+                TcpClient socket = new TcpClient(host, port);
+                NetworkStream netStream = socket.GetStream();
+                netStream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0} " + text);
+
+                bufor = new byte[socket.ReceiveBufferSize];
+                int length = netStream.Read(bufor, 0, socket.ReceiveBufferSize);
+                String dataOut = Encoding.ASCII.GetString(bufor);
+                Console.WriteLine("Received " + host);
+                System.IO.File.WriteAllText(@"C:\Users\alicj\source\repos\Client\odp.txt", dataOut);
+
+            }
+            catch
+            {
+                Console.WriteLine("Something is wrong");
+            }
+
+        }
+    }
+}
